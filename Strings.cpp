3ba120034ba62f73f45c041966c9225b6f92cd0e
Mayank52 <mayank.aggarwal963@gmail.com>
2020-10-18 12:29:25 +0530

    update

--- Strings.cpp
+++ Strings.cpp
@@ -17,7 +17,7 @@ vector<vector<string>> groupWords(vector<string> &words)
     {
         vector<bool> freq(26, 0);
         for (char c : words[i])
-            freq[c - 'a']=true;
+            freq[c - 'a'] = true;
 
         mp[freq].push_back(words[i]);
     }
@@ -33,6 +33,72 @@ vector<vector<string>> groupWords(vector<string> &words)
         for (string str : word)
             cout << str << " ";
         cout << endl;
+    }
+
+    return res;
+}
+
+// 1347. Minimum Number of Steps to Make Two Strings Anagram
+int minSteps(string s, string t)
+{
+    vector<int> freq(26);
+    //count the  freq of characters in str1
+    for (int i = 0; i < s.size(); i++)
+        freq[s[i] - 'a']++;
+
+    int count = 0;
+    //check for the characters in str2, if they have same freq
+    for (int i = 0; i < t.size(); i++)
+    {
+        if (freq[t[i] - 'a'] == 0)
+            count++;
+        else
+            freq[t[i] - 'a']--;
+    }
+
+    return count;
+}
+
+// 890. Find and Replace Pattern
+bool match(string &word, string &pattern)
+{
+    unordered_map<char, char> mp;
+
+    //1.check that all characters of word are mapped to unique values
+    //map all values in word to pattern
+    for (int i = 0; i < word.size(); i++)
+    {
+        //if it present in map but mapped to some other value then return false
+        if (mp.find(word[i]) != mp.end())
+        {
+            if (mp[word[i]] != pattern[i])
+                return false;
+        }
+        //else add it to the map
+        mp[word[i]] = pattern[i];
+    }
+
+    //2. check if all values are mapped to unique keys
+    //check if more than one value has been mapped to same value i.e. more than one key has same value
+    vector<bool> seen(26, false);
+    for (auto ele : mp)
+    {
+        //if the value has already been mapped, then return false
+        if (seen[ele.second - 'a'])
+            return false;
+        //seeing it for first time
+        seen[ele.second - 'a'] = true;
+    }
+
+    return true;
+}
+vector<string> findAndReplacePattern(vector<string> &words, string pattern)
+{
+    vector<string> res;
+    for (string &word : words)
+    {
+        if (match(word, pattern))
+            res.push_back(word);
     }
 
     return res;
